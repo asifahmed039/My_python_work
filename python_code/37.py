42f5abba0f34aafbbf84407c6fe9ffb2561d3df8
Asif Ahmed <asifahmed331999@gmail.com>
2025-12-24 23:21:04 +0530

    Opps part01

---
+++ python_code/37.py
@@ -0,0 +1,21 @@
+#oops in python
+"""
+    To map with real world scenarios, we started using objects in code.
+    This is called object oriented programming.
+     older concept:
+        procedural   line by line
+        function     reusebility and remove redundance from code
+    class ----- object
+
+"""
+#Class & Object in Python
+"""
+    Class is a bluePrint for creating objects.
+"""
+#creating class
+class Student:
+    name="Asif Ahmed"
+
+#Creating object (instance)
+s1=Student()
+print(s1.name)
