8af09c34f1fde13762251cda99fceffc88bc3960
Asif Ahmed <asifahmed331999@gmail.com>
2025-12-30 19:56:24 +0530

    break and continue

--- python_code/23.py
+++ python_code/23.py
@@ -9,10 +9,12 @@ with the next iteration.
 i=0
 while i<=10:
     # i+=1
-    if(i%2==1):
+    if(i%2==1):      #i%2==0 print odd
         # print("hi")
         i+=1
         continue
     print(i)
     i+=1
+
+
     
