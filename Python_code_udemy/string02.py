6053127e66e0068fe43693150507713be30af931
Asif Ahmed <asifahmed331999@gmail.com>
2026-01-15 22:09:26 +0530

    string 06

--- Python_code_udemy/string02.py
+++ Python_code_udemy/string02.py
@@ -18,5 +18,27 @@ print(s5)
 s6='abcdef'
 d='a' in s6  #true if present in s6.
 print(d)
-g='z' not in s6
+g='z' not in s6 #give true if not present in s6.
 print(g)
+
+"""
+    string comparision
+        Dictionary order , Lexicography order
+        ex:--
+            apply>apple  , cat<catch , data>Data, 2nd<Byte 
+
+            0<1<2<3......<A<B<C.....a<b<c<...... Due to Ascii 
+            (48-57)      (65-90)    (97-122)  
+
+"""
+s1='software'
+s2='haedware'
+print(s1==s2)
+print(s1>s2)
+for i in s1:
+    print(i)
+list=[10,5,2,30]
+for i in list:
+    print(i)
+list[0]=50
+print(list[0])
