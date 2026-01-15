05ebbd2fbdaf60e174bff44a9cacbb2c50534131
Asif Ahmed <asifahmed331999@gmail.com>
2026-01-15 22:06:40 +0530

    string

---
+++ Python_code_udemy/String04.py
@@ -0,0 +1,33 @@
+#introduction to string
+"""
+    Hello
+    index may be negative(-) or positive(+)
+
+    length of string:-
+        Bulitin methods in python to find the length of string.
+            len(string_name)
+    
+    Traversing a string:-
+        visiting all index of string with the help of for loop and range() we traverse the string.
+            for i on str_name
+    
+    String Literals:-
+        Single Quotes: 'Hello'
+        Double Quotes: "HEllo"
+        Single Quotes: '''Helo'''
+        
+
+"""
+#sting
+str='Hello'
+#index
+print(str[-1])
+print(len(str))
+#traversing
+for e in str:
+    print(e,end=" ")
+for i in range(len(str)):
+    print(str[i])
+
+
+
