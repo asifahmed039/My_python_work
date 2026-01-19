5448906b79027807c990522e37bbe92705a79bb2
Asif Ahmed <asifahmed331999@gmail.com>
2026-01-19 13:43:11 +0530

    case conversion

---
+++ Python_code_udemy/string10.py
@@ -0,0 +1,31 @@
+#[79]Case Conversion
+"""
+    [01]capitalize()
+        Its convert the first letter to capital letter
+    [02]upper()
+        use to convert the whole string into capital string.
+    [03]lower()
+        use to convert the whole string into lower case.
+    [04]title()
+        its convert every every first letter of string into capital.
+    [05]swapcase()
+        its convert lower into capital and capital into lower.
+    [06]casefold()
+        for all language convert into lower case.
+
+"""
+s1='hello dear'
+s2=s1.capitalize()
+print(s2)
+
+s2=s1.upper()
+print(s2)
+
+s1='hello how are you.'
+s2=s1.title()
+print(s2)
+ 
+s3=s2.swapcase()
+print(s3)
+
+#casef0ld()
